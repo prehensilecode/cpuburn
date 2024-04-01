91b2b31a0495188f0f6edab6a36505597b6e4a81
David Chin <prehensilecode@users.noreply.github.com>
2024-04-01 12:31:43 -0400

    Don't propagate ulimits

--- slurm_cpuburn.sh
+++ slurm_cpuburn.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=all
+# don't propagate ulimits
+#SBATCH --propagate=NONE
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
 #SBATCH --mem-per-cpu=8G
 
 date
